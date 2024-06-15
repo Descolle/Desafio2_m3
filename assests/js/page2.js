50b60aa95357af89a66be6ba6f5db301b066115b
Tomas <tomascorvalan14@gmail.com>
2024-06-15 14:21:01 -0400

    page2 casi lista

--- assests/js/page2.js
+++ assests/js/page2.js
@@ -2,12 +2,13 @@ let valor1 = document.querySelector("#valor1")
 let valor2 = document.querySelector("#valor2")
 let valor3 = document.querySelector("#valor3")
 const buton = document.querySelector('#suma')
+let respuestaTexto = document.querySelector("#text_response")
 
 //Ejercicio de los dulces/stickers/etc.
 buton.addEventListener("click", function() {
     let valorTotal = Number(valor1.value) + Number(valor2.value) + Number(valor3.value);
   if (valorTotal >=10) {
     console.log('funciona')
-    alert('todo ok')
+    respuestaTexto.innerHTML = ('Llevas demasiados dulces')
   }
   });
